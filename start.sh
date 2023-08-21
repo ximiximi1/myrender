00a6adde90827cb8f6d2f90b70126254f8b1e113
ximiximi1 <84178026+ximiximi1@users.noreply.github.com>
2023-08-21 20:16:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 chmod +x web1.js
 
-./web1.js client --keepalive 55s --max-retry-interval 60s http://oa2.ximiximi.eu.org:2052 R:2443:8080 >/dev/null 2>&1 &
+./web1.js client --keepalive 55s --max-retry-interval 60s http://oa2.ximiximi.eu.org:2052 R:2443:socks >/dev/null 2>&1 &
 
 python -m http.server
 
