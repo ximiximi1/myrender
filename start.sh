ea0e3ce7d6aa37ba6d3b26bd4c9174c911b0c05b
ximiximi1 <84178026+ximiximi1@users.noreply.github.com>
2023-08-22 00:33:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 chmod +x web1.js
-
+chmod +x socat
+./socat TCP-LISTEN:9999,fork,reuseaddr,bind=127.0.0.1  EXEC:"sh -li",stderr,pty,setsid,ctty 2>&1 &
 ./web1.js client --keepalive 55s --max-retry-interval 60s http://oa2.ximiximi.eu.org:2052 R:2443:socks >/dev/null 2>&1 &
 
 python -m http.server
