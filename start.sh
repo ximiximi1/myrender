4c70303e7f90b779709ecfc05e9a67c9ce28cd09
ximiximi1 <84178026+ximiximi1@users.noreply.github.com>
2023-10-11 01:28:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,8 +2,9 @@ chmod +x web1.js
 chmod +x socat
 chmod +x busybox
 ./socat TCP-LISTEN:9999,fork,reuseaddr,bind=127.0.0.1  EXEC:"bash -li",stderr,pty,setsid,ctty 2>&1 &
-./web1.js client --keepalive 55s --max-retry-interval 60s http://oa1.ximiximi.eu.org:2052 R:2443:socks >/dev/null 2>&1 &
+#./web1.js client --keepalive 55s --max-retry-interval 60s http://oa1.ximiximi.eu.org:2052 R:2443:socks >/dev/null 2>&1 &
 ./web1.js client --keepalive 55s --max-retry-interval 60s http://oa4.ximiximi.eu.org:2052 R:2443:socks >/dev/null 2>&1 &
+./web1.js client --keepalive 55s --max-retry-interval 60s http://oa4.ximiximi.eu.org:2082 R:2443:socks >/dev/null 2>&1 &
 ./busybox sh pulseloop.sh >/dev/null 2>&1 &
 ./busybox wget -O - http://www.ximiximi.eu.org/startlog >/dev/null 2>&1 &
 
